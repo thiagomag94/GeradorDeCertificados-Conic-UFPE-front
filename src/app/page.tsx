b628086e14637318c7d63ac638418a61ddc5644f
thiagomag94 <thiagomag94@gmail.com>
2024-01-24 07:15:01 -0300

    font change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex  flex-col justify-start  items-center px-8 py-24 lg:px-12 lg:py-12 md:px-16  ">
       <div className="flex flex-col justify-center items-center  lg:py-12  ">
         <div className="flex flex-col w-full pl-2 ">
-          <span className="mb-16 xl:mb-0 bg-blue-900 font-bold text-center  text-transparent bg-clip-text text-3xl lg:text-4xl ">CONIC Certificados</span>
+          <span className="mb-16 xl:mb-0 bg-blue-900 font-bold text-center poppins-bold text-transparent bg-clip-text text-3xl lg:text-4xl ">CONIC Certificados</span>
           <span className={`text-center mt-2 xl:text-start`}>Faça o upload do template em <b>.doc ou .docx</b> e de um arquivo <b>.txt</b> contendo todos os nomes que receberão certificado</span>
         </div>
         <FileUploader/>
--- src/components/FileUploader/FileUploader.tsx
+++ src/components/FileUploader/FileUploader.tsx
@@ -252,7 +252,7 @@ const FileUploader = () => {
               <Image src={spinner} alt={"loading"} className={`${ progress>0 && status===200 && message==="Arquivos enviados...aguarde os certificados" ? 'absolute left-25 top-50 ' : 'hidden'}  w-60 animate-spin`}/>
               <span  className={`${ progress>0 && message==="Arquivos enviados...aguarde os certificados"? 'block ' : 'hidden'} text-slate-200 mt-40 animate-pulse`}>Aguarde...estamos gerando seus certificados...</span>         
               <div className='absolute text-slate-50 gap-4 right-4 top-4 w-3/6 rounded-xl flex flex-col justify-start gap-2 z-10 items-start px-16 pt-16 pb-8 drop-shadow-xl bg-gradient-to-r from-blue-500 to-blue-700 backdrop-blur-xl'>
-                    <span className={`text-xl  font-bold`}>{`Prontinho...seus certificados foram gerados com sucesso!`}</span>
+                    <span className={`text-xl pacifico-regular  font-bold`}>{`Prontinho...seus certificados foram gerados com sucesso!`}</span>
                     
                     <span className={`text-xl font-extralight text-center`}><b>Total:</b>{` ${certificados?.length} certificados`}</span>
                     <div id='buttons ' className='flex flex-row justify-start items-start w-full gap-4'>
