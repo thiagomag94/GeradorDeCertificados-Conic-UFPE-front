98c69b7b43d62109a08cd1f8f061d56bbac1d8c0
thiagomag94 <thiagomag94@gmail.com>
2024-01-26 11:08:30 -0300

    changes logic from component file group

--- src/components/FilesGroup/FileGroup.tsx
+++ src/components/FilesGroup/FileGroup.tsx
@@ -7,9 +7,9 @@ const FilesGroup = (props:any) => {
     
     return (
           <div className={` ${alerta==='escolhido' ? '' : 'hidden'} border-2 border-cyan-600/10 flex  justify-center items-center w-full bg-blue-600/60 text-slate-50 rounded-lg p-4 mt-4`}>
-            {alerta==='escolhido' && <File title={titleTxt} image={txtImage} />}
-            {alerta==='escolhido' && <File title={titleDoc} image={docImage} />}
-            {alerta==='escolhido' && <File title={titleExcel} image={excelImage} />}
+            {titleTxt && <File title={titleTxt} image={txtImage} />}
+            {titleDoc && <File title={titleDoc} image={docImage} />}
+            {titleExcel && <File title={titleExcel} image={excelImage} />}
              
           </div>
           
